2d950255da47c7f351eccf82b35a09af2b962d16
Nithin prathapan <nithinprathapan32567@gmail.com>
2026-04-07 09:46:23 +0530

    podfile updated

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -17,11 +17,14 @@ export PATH="$PATH:`pwd`/flutter/bin"
 
 # Precache and install dependencies.
 echo "Preparing Flutter environment..."
+flutter clean
 flutter precache --ios
 flutter pub get
 
 # Setup Flutter configuration.
 echo "Building iOS configuration..."
+# Disable parallel code signing to avoid Code 70 internal errors in Xcode Cloud
+export COCOAPODS_PARALLEL_CODE_SIGN=false
 # Change to --release to ensure App.framework is fully built for the App Store.
 flutter build ios --release --no-codesign
 
